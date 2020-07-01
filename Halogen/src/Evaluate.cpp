6d69c3529e7c6ba4bcd319bf5c25c40a0e9e00ab
Kieren Pearson <kierenpearson9@gmail.com>
2020-07-02 01:22:39 +1000

    Improved draw material detection
    Bench: 15349767

--- Halogen/src/Evaluate.cpp
+++ Halogen/src/Evaluate.cpp
@@ -259,37 +259,77 @@ bool InsufficentMaterialEvaluation(const Position& position)
 
 		According to the rules of a dead position, Article 5.2 b, when there is no possibility of checkmate for either side with any series of legal moves, the position is an immediate draw if 
 
-		- both sides have a bare king																																									1.
-		- one side has a king and a minor piece against a bare king																																		1.
-		- both sides have a king and a bishop, the bishops being the same color																															1.
+		- both sides have a bare king																																									
+		- one side has a king and a minor piece against a bare king																																		
+		- both sides have a king and a bishop, the bishops being the same color																															
 
 		The bishops of different colors are not counted as an immediate draw, because of the possibility of a helpmate in the corner.
 		Since this is unlikely given even a four ply search, we may introduce another class of drawn positions: those that cannot be claimed, but can be evaluated as draws:
 
-		- two knights against the bare king																																								2.
-		- both sides have a king and a minor piece each																																					1.
-		- the weaker side has a minor piece against two knights																																			2.
-		- two bishops draw against a bishop																																								4.
-		- two minor pieces against one draw, except when the stronger side has a bishop pair																											3.
+		- two knights against the bare king																																								
+		- both sides have a king and a minor piece each																																					
+		- the weaker side has a minor piece against two knights																																			
+		- two bishops draw against a bishop																																								
+		- two minor pieces against one draw, except when the stronger side has a bishop pair																											
 
 		Please note that a knight or even two knights against two bishops are not included here, as it is possible to win this ending.
 	*/
 
-	//We know the board must contain just knights, bishops and kings
 	int WhiteBishops = GetBitCount(position.GetPieceBB(WHITE_BISHOP));
 	int BlackBishops = GetBitCount(position.GetPieceBB(BLACK_BISHOP));
 	int WhiteKnights = GetBitCount(position.GetPieceBB(WHITE_KNIGHT));
 	int BlackKnights = GetBitCount(position.GetPieceBB(BLACK_KNIGHT));
-	int WhiteMinor = WhiteBishops + WhiteKnights;
-	int BlackMinor = BlackBishops + BlackKnights;
+	int WhitePawns = GetBitCount(position.GetPieceBB(WHITE_PAWN));
+	int BlackPawns = GetBitCount(position.GetPieceBB(BLACK_PAWN));
 
-	if (WhiteMinor <= 1 && BlackMinor <= 1) return true;												//1		
-	if (WhiteMinor <= 1 && BlackKnights <= 2 && BlackBishops == 0) return true;							//2		KNvKNN, KBvKNN, KvKNN or combinations with less nights
-	if (BlackMinor <= 1 && WhiteKnights <= 2 && WhiteBishops == 0) return true;							//2		
-	if (WhiteMinor <= 1 && BlackMinor <= 2 && BlackBishops < 2) return true;							//3		
-	if (BlackMinor <= 1 && WhiteMinor <= 2 && WhiteBishops < 2) return true;							//3
-	if (WhiteBishops == 1 && BlackBishops == 2 && WhiteKnights == 0 && BlackKnights == 0) return true;	//4
-	if (BlackBishops == 1 && WhiteBishops == 2 && BlackKnights == 0 && WhiteKnights == 0) return true;	//4
+	int pieces = 2 + WhiteBishops + BlackBishops + WhiteKnights + BlackKnights + WhitePawns + BlackPawns;
+
+	if (pieces == 2)
+	{
+		return true;																//KvK
+	}
+
+	if (pieces == 3)
+	{
+		if (WhitePawns + BlackPawns == 0) 
+			return true;															//KBvK or KNvK
+	}
+
+	if (pieces == 4)
+	{
+		if ((WhiteKnights | WhiteBishops) && BlackPawns)							//KNvKP or KBvKP
+			return true;							
+
+		if ((BlackKnights | BlackBishops) && WhitePawns)
+			return true;							
+
+		if ((WhiteKnights | WhiteBishops) && (BlackKnights | BlackBishops))			//KNvKB, KBvKB, KNvKN
+			return true;							
+
+		if (WhiteKnights == 2 || BlackKnights == 2)									//KNNvK
+			return true;
+	}
+
+	if (pieces == 5)
+	{
+		if ((WhiteKnights | WhiteBishops | WhitePawns) && BlackKnights == 2)							//KBvKNN, KNvKNN, KPvKNN
+			return true;
+
+		if ((BlackKnights | BlackBishops | BlackPawns) && WhiteKnights == 2)
+			return true;
+
+		if (WhiteKnights + WhiteBishops == 1 && BlackKnights == 1 && BlackBishops == 1)					//KBvKNB, KNvKNB
+			return true;
+
+		if (BlackKnights + BlackBishops == 1 && WhiteKnights == 1 && WhiteBishops == 1)
+			return true;
+
+		if (WhiteBishops == 1 && BlackBishops == 2)														//KBvKBB
+			return true;
+
+		if (WhiteBishops == 2 && BlackBishops == 1)														
+			return true;
+	}
 
 	return false;
 }
