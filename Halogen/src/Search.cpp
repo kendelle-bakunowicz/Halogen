8f828f094dd896d34c9bd45d91c551ecce4b9739
Kieren Pearson <kierenpearson9@gmail.com>
2020-08-17 01:49:11 +1000

    Attempt 2
    Bench: 5797880

--- Halogen/src/Search.cpp
+++ Halogen/src/Search.cpp
@@ -448,7 +448,7 @@ SearchResult NegaScout(Position& position, unsigned int initialDepth, int depthR
 		position.RevertNullMove();
 
 		//Verification search worth about ~5 elo. 
-		if (score + 2 * TempoBonus >= beta)
+		if (score + TempoBonus >= beta)
 		{
 			SearchResult result = NegaScout(position, initialDepth, depthRemaining - reduction - 1, beta - 1, beta, colour, distanceFromRoot, false, locals, sharedData);
 
