2ddf2bba6cff872e1a69a2926b149570ce4a1262
Kieren Pearson <kierenpearson9@gmail.com>
2020-09-08 00:58:11 +1000

    Bench: 7272832

--- Halogen/src/Position.cpp
+++ Halogen/src/Position.cpp
@@ -1,6 +1,6 @@
 #include "Position.h"
 
-Position::Position() : net(InitNetwork("C:\\HalogenWeights\\RNC1cVNovG.network"))
+Position::Position() : net(InitNetwork("C:\\HalogenWeights\\EqErDSCFCs.network"))
 {
 	key = EMPTY;
 	NodeCount = 0;
