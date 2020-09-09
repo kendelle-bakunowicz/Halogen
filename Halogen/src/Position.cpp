c9495b4776e197808cff86c1432a9dc7f61cf45c
Kieren Pearson <kierenpearson9@gmail.com>
2020-09-09 14:26:32 +1000

    Bench: 6474994

--- Halogen/src/Position.cpp
+++ Halogen/src/Position.cpp
@@ -1,6 +1,6 @@
 #include "Position.h"
 
-Position::Position() : net(InitNetwork("C:\\HalogenWeights\\uAdi7tSlFB.network"))
+Position::Position() : net(InitNetwork("C:\\HalogenWeights\\IkuNXX9m1j.network"))
 {
 	key = EMPTY;
 	NodeCount = 0;
