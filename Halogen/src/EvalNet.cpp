5e4ae36361a4cb7478457a9514d48c91d7f11736
Kieren Pearson <kierenpearson9@gmail.com>
2020-10-25 02:09:10 +1100

    Bench: 6522519

--- Halogen/src/EvalNet.cpp
+++ Halogen/src/EvalNet.cpp
@@ -9,7 +9,7 @@ int EvaluatePositionNet(Position& position, EvalCacheTable& evalTable)
 
     if (!evalTable.GetEntry(position.GetZobristKey(), eval))
     {
-        eval = position.GetEvaluation();
+        eval = position.GetEvaluation() * 2;
         evalTable.AddEntry(position.GetZobristKey(), eval);
     }
 
