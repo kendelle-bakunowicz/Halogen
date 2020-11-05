e41d61692fb0c0bd9dd18911936ce7644205e543
Kieren Pearson <kierenpearson9@gmail.com>
2020-11-05 14:09:26 +1100

    Try multiplier of 1.7
    Bench: 6012928

--- Halogen/src/EvalNet.cpp
+++ Halogen/src/EvalNet.cpp
@@ -11,7 +11,7 @@ int EvaluatePositionNet(Position& position, EvalCacheTable& evalTable)
 
     if (!evalTable.GetEntry(position.GetZobristKey(), eval))
     {
-        eval = position.GetEvaluation() * 1.35 + (position.GetTurn() == WHITE ? TEMPO : -TEMPO);
+        eval = position.GetEvaluation() * 1.7 + (position.GetTurn() == WHITE ? TEMPO : -TEMPO);
         evalTable.AddEntry(position.GetZobristKey(), eval);
     }
 
