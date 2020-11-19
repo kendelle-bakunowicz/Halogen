0ce45183c45bd7815aafb06e7a01f9c291c10355
Kieren Pearson <kierenpearson9@gmail.com>
2020-11-20 02:28:51 +1100

    Bench: 7274496

--- Halogen/src/Search.cpp
+++ Halogen/src/Search.cpp
@@ -103,7 +103,7 @@ void InitSearch()
 {
 	KeepSearching = true;
 
-	int Futility_linear = 25;
+	int Futility_linear = 50;
 	int Futility_constant = 100;
 
 	for (int i = 0; i < FutilityMaxDepth; i++)
