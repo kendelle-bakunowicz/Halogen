6ad39b61a8046ce9d9b12460bbc8ec7e11dab2b4
Kieren Pearson <kierenpearson9@gmail.com>
2020-12-03 08:39:47 +1100

    Bench: 6914048

--- Halogen/src/Search.cpp
+++ Halogen/src/Search.cpp
@@ -2,8 +2,8 @@
 
 /*Search constants*/
 
-double LMR_constant = 0.5;
-double LMR_coeff    = 0.5;
+double LMR_constant = -1.35;
+double LMR_coeff    = 0.87;
 
 unsigned int R = 3;					//Null-move reduction depth
 unsigned int VariableNullDepth = 7;	//Beyond this depth R = 4
